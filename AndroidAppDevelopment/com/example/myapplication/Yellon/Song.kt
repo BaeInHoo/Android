63a9cec8aeb0e3ab9fec65cf15877319d7165e7b
privateMiss <95017292+privateMiss@users.noreply.github.com>
2022-03-02 03:26:13 +0900

    Delete AndroidAppDevelopment/com/example/myapplication/Yellon directory

--- AndroidAppDevelopment/com/example/myapplication/Yellon/Song.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.Yellon
-
-import java.io.Serializable
-
-class Song (
-    val title : String? = null,
-    val thumbnail : String? = null,
-    val song : String? = null
-    ):Serializable
