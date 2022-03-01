e74a710a53e5ad6b287969ed6c8cad409dec932e
privateMiss <95017292+privateMiss@users.noreply.github.com>
2022-03-01 11:58:07 +0900

    Add files via upload

---
+++ AndroidAppDevelopment/com/example/myapplication/Yellon/Song.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication.Yellon
+
+import java.io.Serializable
+
+class Song (
+    val title : String? = null,
+    val thumbnail : String? = null,
+    val song : String? = null
+    ):Serializable
