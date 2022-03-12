b0d73cfc1741eb653e3a1c1c1d53081e0fee816b
privateMiss <95017292+privateMiss@users.noreply.github.com>
2022-03-12 20:48:17 +0900

    Add files via upload

---
+++ practice/com/example/demo/MyTube/Mytube.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.Mytube
+
+import java.io.Serializable
+
+class Mytube (
+    var title: String? = null,
+    var content: String? = null,
+    var thumbnail : String? = null,
+    var video : String? = null
+        ):Serializable
